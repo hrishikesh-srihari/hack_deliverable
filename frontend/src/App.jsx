c5b914c091a073df502235afd81c79a4310b0ce1
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-04-09 23:04:45 -0700

    added form submission handler to eliminate page reloading on form submission. This should allow the quotes to update automatically to the list at the bottom of the page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 function App() {
 	const [quotes, setQuotes] = useState([]);
+	const [name, setName] = useState('');
+	const [message, setMessage] = useState('');
 
 	useEffect(() => {
 		const fetchQuotes = async () => {
@@ -21,6 +23,31 @@ function App() {
 
 		fetchQuotes();
 	}, []);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+		const response = await fetch('/api/quote', {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: new URLSearchParams({ name, message }),
+		});
+
+		if (response.ok) {
+			const newQuote = await response.json();
+			setQuotes([...quotes, newQuote]);
+			setName('');
+			setMessage('');
+		} else {
+			console.error('Error submitting quote:', response.status);
+		}
+		} catch (error) {
+		console.error('Error submitting quote:', error);
+		}
+	};
 
 
 	return (
