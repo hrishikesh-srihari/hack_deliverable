7383a87eb1294f8f92d00fe672364d4a60001bf9
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-04-09 22:06:54 -0700

    Added functionality for getting quotes from database by max age

--- api/src/app.py
+++ api/src/app.py
@@ -1,22 +1,17 @@
-from datetime import datetime
-from typing import TypedDict
-
-from fastapi import FastAPI, Form, status
-from fastapi.responses import RedirectResponse
-
+from datetime import datetime, timedelta
+from typing import TypedDict, List, Optional
+from fastapi import FastAPI, Form, Query, status
+from fastapi.responses import RedirectResponse, JSONResponse
 from services.database import JSONDatabase
 
 app = FastAPI()
-
 
 class Quote(TypedDict):
     name: str
     message: str
     time: str
 
-
 database: JSONDatabase[list[Quote]] = JSONDatabase("data/database.json")
-
 
 @app.on_event("startup")
 def on_startup() -> None:
@@ -34,17 +29,26 @@ def on_shutdown() -> None:
 
 @app.post("/quote")
 def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
-    """
-    Process a user submitting a new quote.
-    You should not modify this function except for the return value.
-    """
+    """Process a user submitting a new quote."""
     now = datetime.now().replace(microsecond=0)
-
     quote = Quote(name=name, message=message, time=now.isoformat())
     database["quotes"].append(quote)
-
-    # You may modify the return value as needed to support other functionality
-    return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
+    return JSONResponse(quote, status_code=status.HTTP_201_CREATED)
 
 
-# TODO: add another API route with a query parameter to retrieve quotes based on max age
+@app.get("/quotes")
+def get_quotes(max_age: Optional[int] = None) -> JSONResponse:
+    """
+    Retrieve quotes from the database.
+    
+    Args:
+        max_age (int, optional): Maximum age of quotes in days. If not provided, all quotes will be returned.
+    
+    Returns:
+        JSONResponse: A JSON response containing the list of quotes.
+    """
+    quotes = database["quotes"]
+    if max_age is not None:
+        cutoff = datetime.now() - timedelta(days=max_age)
+        quotes = [quote for quote in quotes if datetime.fromisoformat(quote["time"]) >= cutoff]
+    return JSONResponse(quotes)
