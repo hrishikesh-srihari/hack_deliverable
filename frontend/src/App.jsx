c080a069d78d997496b02b772d79b8911fb8de14
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-04-09 23:10:44 -0700

    Linked form submission handler to a form to test functionality. Going to add functionality for users to query by date.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,12 +56,25 @@ function App() {
 			<h1>Hack at UCI Tech Deliverable</h1>
 
 			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
+				<input
+				type="text"
+				name="name"
+				id="input-name"
+				required
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				/>
 				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
+				<input
+				type="text"
+				name="message"
+				id="input-message"
+				required
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+				/>
 				<button type="submit">Submit</button>
 			</form>
 
